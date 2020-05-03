c6ff87a2831782b92db9c1ce5d74adb0dfa879db
Johan Mora <joenmodu99@gmail.com>
2020-05-03 03:29:32 -0400

    Carousel Implementado

--- src/sections/Portfolio/components/CarouselItem.js
+++ src/sections/Portfolio/components/CarouselItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Yasuo from '../../../assets/Yasuo.jpg'
-export const CarouselItem = () => {
+export const CarouselItem = props => {
+    const {image, title, link, text} = props
     return <div className="portfolio-carousel-item rounded">
-        <img className="portfolio-carousel-item-image" src={Yasuo}/>
-        <a className="portfolio-carousel-item-title" href='#'>Yasuo</a>
-        <p className="portfolio-carousel-item-text">Yasuo</p>
+        <img className="portfolio-carousel-item-image rounded" src={image || Yasuo}/>
+        <a className="portfolio-carousel-item-title" href={link}>{title || 'Yasuo'}</a>
+        <p className="portfolio-carousel-item-text">{text || 'Yasuo'}</p>
     </div>
 }
--- src/sections/Portfolio/containers/Portfolio.js
+++ src/sections/Portfolio/containers/Portfolio.js
@@ -1,5 +1,44 @@
 import React from 'react'
-import {Carousel} from '../components/Carousel'
+import Slider from "react-slick";
+import {CarouselItem} from '../components/CarouselItem'
+import platzivideo from '../../../assets/Platzivideo.png'
+import ticketSystem from '../../../assets/TicketSystem.png'
+import smartphones from '../../../assets/Smartphones.png'
+import product from '../../../assets/Product.png'
+import restaurant from '../../../assets/restaurant.png' 
+import petgram from '../../../assets/petgram.png'
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "#FF703F", borderRadius: '40px'}}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "#FF703F", borderRadius: '40px'}}
+        onClick={onClick}
+      />
+    );
+  }
+
+const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 700,
+    cssEase: "ease-out",
+    nextArrow: <SampleNextArrow/>,
+    prevArrow: <SamplePrevArrow/>
+  };
 
 export const Portfolio = () => {
     return <div className="portfolio-container">
@@ -7,10 +46,15 @@ export const Portfolio = () => {
             <h2 className="portfolio-subtitle text-orange">Portfolio</h2>
             <h1 className="portfolio-title">Lastest Projects</h1>
         </div>
-        <Carousel/>
-        <div className="portfolio-controls">
-            <a href="#"><i className="fas fa-angle-left"></i></a>
-            <a href="#"><i className="fas fa-angle-right"></i></a>
+        <div className="portfolio-carousel">
+            <Slider {...settings} className="portfolio-carousel-slider">
+                <CarouselItem image={restaurant} title={'Your Restaurant'} link={'https://aujemd.github.io/Your-Restaurant/'} text={'Web Design - HTML/CSS'} index={1}/>
+                <CarouselItem image={smartphones} title={'Smartphones Page'} link={'https://aujemd.github.io/Website-smartphones/'} text={'Web Design - HTML/CSS'} index={2}/>
+                <CarouselItem image={product} title={'Book Page'} link={'https://aujemd.github.io/Website-Product/'} text={'Web Design - HTML/CSS'} index={3}/>
+                <CarouselItem image={platzivideo} title={'PlatziVideo'} link={'https://platzi-video-topaz.now.sh/'} text={'Fronted Development - React.js'}index={4}/>
+                <CarouselItem image={petgram} title={'Petgram'} link={'https://petgram-tan-nu.now.sh/'} text={'Fronted Development - React.js'}index={5}/>
+                <CarouselItem image={ticketSystem} title={'Ticket System'} link={'https://app-system-of-tickets.herokuapp.com/'} text={'Backend Development - HTML/PHP'}index={6}/>
+            </Slider>
         </div>
     </div>
 }
