459b13d42751aab2e3353b8e2431b438a660a576
Johan Mora <joenmodu99@gmail.com>
2020-05-03 19:58:15 -0400

    Links añadidos a los enlaces

--- src/sections/Footer/containers/Footer.js
+++ src/sections/Footer/containers/Footer.js
@@ -6,8 +6,8 @@ export const Footer = () => {
         <ul className="footer-social">
             <li><a href="https://www.facebook.com/jemd1999"className="footer-social-item"><i className="fab fa-facebook-f"></i></a></li>
             <li><a href="https://twitter.com/johanEmd"className="footer-social-item"><i className="fab fa-twitter"></i></a></li>
-            <li><a href="#"className="footer-social-item"><i className="fab fa-instagram"></i></a></li>
-            <li><a href="#"className="footer-social-item"><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a href="https://www.instagram.com/jemdu99/"className="footer-social-item"><i className="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/johan-mora-925421197/"className="footer-social-item"><i className="fab fa-linkedin-in"></i></a></li>
         </ul>
         <h1 className="footer-email">joenmodu99@gmail.com</h1>
         <ul className="footer-nav">
@@ -16,6 +16,6 @@ export const Footer = () => {
         <li><a href="#" className="footer-nav-item">Services</a></li>
         <li><a href="#" className="footer-nav-item">Portfolio</a></li>
         </ul>
-    <p className="footer-firm">Developed by <a href='#'>@Aujemd</a></p>
+    <p className="footer-firm">Developed by <a href='https://github.com/Aujemd'>@Aujemd</a></p>
     </div>
 }
--- src/sections/Home/components/Social.js
+++ src/sections/Home/components/Social.js
@@ -4,9 +4,9 @@ export const Social = () => {
     return <div className="home-social">
         <ul>
             <li><a href="https://www.facebook.com/jemd1999"><i className="fab fa-facebook-f"></i></a></li>
-            <li><a href="#"><i className="https://twitter.com/johanEmd"></i></a></li>
-            <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-            <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a href="https://twitter.com/johanEmd"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/jemdu99/"><i className="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/johan-mora-925421197/"><i className="fab fa-linkedin-in"></i></a></li>
         </ul>
     </div>
 }
