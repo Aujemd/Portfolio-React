c27e3ae9dd0845ab11a7f74b7934472ee0f69561
Johan Mora <joenmodu99@gmail.com>
2020-05-05 04:18:59 -0400

    Animaciones implementadas

--- src/sections/About/components/Content.js
+++ src/sections/About/components/Content.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react'
+import Aos from "aos"
+import 'aos/dist/aos.css'
 
 export const Content = () => {
-    return <div className="about-content">
+    useEffect(() => {
+        Aos.init({duration: 2000})
+    }, [])
+    return <div className="about-content" data-aos="fade-left">
     <h2 className="about-content-subtitle">About</h2>
     <h1 className="about-content-title">Why You Hire Me?</h1>
     <p className="about-content-text">I'm Web developer in progress with management of Fronted and Backend Technologies. Knowledge in
--- src/sections/About/containers/About.js
+++ src/sections/About/containers/About.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Content} from '../components/Content'
 import {Stats} from '../components/Stats'
+import Aos from "aos"
+import 'aos/dist/aos.css'
+import img from '../../../assets/undraw_about_me_wa29.svg'
 
 export const About = () =>{
+    useEffect(() => {
+        Aos.init({duration: 2000})
+    }, [])
     return<div className="about-container" id="about">
-        <div className="about-hero"></div>
+        <img src={img}className="about-hero" data-aos="fade-right"/>
         <Content/>
         <Stats/>
     </div>
